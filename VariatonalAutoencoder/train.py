128f641bdc43c324d8f99d490828e41ccd154fca
proddxterner <156083281+proddxterner@users.noreply.github.com>
2024-10-20 20:28:29 +0200

    Variational Autoencoder
    Prepocess.py for preprocessing dataset from .wav format into Spectrograms in .npy
    Train.py to essentially train the VAE with the dataset which already completed preprocess.py
    Autoencoder.py the new updated code to convert the previous AE into a VAE including probibalistic features (not fully functional yet)
    Spectrograms.zip Preprocessed test dataset converted into spectrograms

---
+++ VariatonalAutoencoder/train.py
@@ -0,0 +1,44 @@
+import os
+
+import numpy as np
+
+from autoencoder import VAE
+
+
+LEARNING_RATE = 0.0005
+BATCH_SIZE = 64
+EPOCHS = 150
+
+SPECTROGRAMS_PATH = "C:/Users/Deterner/Dropbox/beats/! - AA Maturarbeit/Dataset/Snares/Spectograms"
+
+
+def load_dss(spectrograms_path):
+    x_train = []
+    for root, _, file_names in os.walk(spectrograms_path):
+        for file_name in file_names:
+            file_path = os.path.join(root, file_name)
+            spectrogram = np.load(file_path) # (n_bins, n_frames, 1)
+            x_train.append(spectrogram)
+    x_train = np.array(x_train)
+    x_train = x_train[..., np.newaxis] # -> (10006/num_samples, 256, 64, 1)
+    return x_train
+
+
+def train(x_train, learning_rate, batch_size, epochs):
+    autoencoder = VAE(
+        input_shape=(256, 64, 1),
+        conv_filters=(512, 256, 128, 64, 32),
+        conv_kernels=(3, 3, 3, 3, 3),
+        conv_strides=(2, 2, 2, 2, (2, 1)),
+        latent_space_dim=128
+    )
+    autoencoder.summary()
+    autoencoder.compile(learning_rate)
+    autoencoder.train(x_train, batch_size, epochs)
+    return autoencoder
+
+
+if __name__ == "__main__":
+    x_train = load_dss(SPECTROGRAMS_PATH)
+    autoencoder = train(x_train, LEARNING_RATE, BATCH_SIZE, EPOCHS)
+    autoencoder.save("model")
