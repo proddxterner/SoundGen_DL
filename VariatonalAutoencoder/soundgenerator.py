7f785198e13beca3f01033cbe5970e1d4f675835
proddxterner <156083281+proddxterner@users.noreply.github.com>
2024-12-26 17:01:11 +0100

    Update soundgenerator.py

--- VariatonalAutoencoder/soundgenerator.py
+++ VariatonalAutoencoder/soundgenerator.py
@@ -1,6 +1,6 @@
 # 1- Build SoundGenerator class
 # 2- implement generate.py script
-from preprocess import min_max_normaliser
+from preprocess import MinMaxNormaliser
 import librosa
 
 class SoundGenerator:
@@ -9,7 +9,7 @@ class SoundGenerator:
     def __init__(self, vae, hop_length):
         self.vae = vae
         self.hop_length = hop_length
-        self.min_max_normaliser = min_max_normaliser(0, 1)
+        self.min_max_normaliser = MinMaxNormaliser(0, 1)
 
 
     def generate(self, spectrograms, min_max_values):
@@ -23,12 +23,11 @@ class SoundGenerator:
             # reshape log spectrogram
             log_spectrogram = spectrogram[:, :, 0] # copying first 2 dimension and dropping 3rd
             # denormalising 
-            denorm_log_spec = self.min_max_normaliser.denormalize(log_spectrogram, min_max_value["min"], min_max_value["max"])
+            denorm_log_spec = self.min_max_normaliser.denormalise(log_spectrogram, min_max_value["min"], min_max_value["max"])
             # log spectrogram to linear spectrogram - from dB to normal amplitude
             spec = librosa.db_to_amplitude(denorm_log_spec)
             # apply inverse short time fourier transform
-            signal = librosa.istft(spec, self.hop_length)
+            signal = librosa.istft(spec, hop_length=self.hop_length)
             # append to list "signals"
             signals.append(signal)
         return signals
-
